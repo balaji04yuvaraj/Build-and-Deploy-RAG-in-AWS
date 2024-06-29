ee0d4ae3381f965b23beb83c7edbff42a87086e8
unknown <sribalajih@gmail.com>
2024-06-30 00:19:24 +0530

    stable diffucion addded v1

--- main1.py
+++ main1.py
@@ -2,26 +2,32 @@ import os
 import json
 import sys
 import boto3
+import base64
+
 
 print("imported successfully...")
 
 prompt="""
-
-        you are a cricket expert now just tell me when RCB will win the IPL?
+     provide me one 4k hd image of leonardo da vinci playing cricket. 
 """
+
+prompt_template=[{"text":prompt,"weight":1}]
+
 
 bedrock=boto3.client(service_name="bedrock-runtime")
 
 payload={
     
-    "prompt": "[INST]"+prompt+"[/INST]",
-    "max_gen_len": 512,
-    "temperature": 0.3,
-    "top_p":0.9
+   "text_prompts":prompt_template,
+    "cfg_scale":10,
+    "seed":0,
+    "steps":50,
+    "width":512,
+    "height":512
 }
 
 body=json.dumps(payload)
-model_id="mistral.mistral-7b-instruct-v0:2"
+model_id="stability.stable-diffusion-xl-v1"
 
 
 response=bedrock.invoke_model(
@@ -29,8 +35,22 @@ response=bedrock.invoke_model(
     modelId=model_id,
     accept="application/json",
     contentType="application/json"
+    
 )
 
 response_body=json.loads(response.get("body").read())
-response_text=response_body["generation"]
-print(response_text)
+# response_text=response_body["generation"]
+print(response_body)
+
+artifact = response_body.get("artifacts")[0]
+image_encoded = artifact.get("base64").encode('utf-8')
+image_bytes = base64.b64decode(image_encoded)
+
+output_dir = "output"
+os.makedirs(output_dir,exist_ok=True)
+file_name=f"{output_dir}/generated-img.png"
+
+with open(file_name,"wb") as f:
+    f.write(image_bytes)
+    
+    
