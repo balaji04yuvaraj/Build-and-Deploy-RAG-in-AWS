581a032edea750fff06162215b43f09ea90a41e9
unknown <sribalajih@gmail.com>
2024-06-30 22:44:02 +0530

    updated the script1

---
+++ app.py
@@ -0,0 +1,13 @@
+import json
+import os
+import sys
+import boto3
+import streamlit as streamlit
+
+from langchain_community.embeddings import BedrockEmbedding
+from langchain.llms.bedrock import Bedrock
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrevalQA
+from langchain.vectorstores import FAISS
+
